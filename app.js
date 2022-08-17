14434926c4efde726857df2a928a1ee6daba757f
Dorian Cerfontaine <61007463+cerfontaine@users.noreply.github.com>
2022-08-18 01:02:05 +0200

    first commit - portfolio - github pages

---
+++ app.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var popup = document.getElementById("navbar-default");
+    //var changeicon = document.getElementById("changeicon");
+    //changeicon.classList.toggle("fa-times");
+    popup.classList.toggle("show");
+}
